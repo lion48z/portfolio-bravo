4a5c9bde19dd28671bbd324249b35a3dd9a34bf0
lion48z <140863653+lion48z@users.noreply.github.com>
2023-10-14 13:51:47 -0500

    adding bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
+//import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
+      {/*<div>
+      <Navbar/>text 
+    </div>*/}
      
    
    
