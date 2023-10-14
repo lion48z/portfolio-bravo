8cce9b8e2dc467e29ce3061364778aeb030c2649
lion48z <140863653+lion48z@users.noreply.github.com>
2023-10-14 18:16:17 -0500

    Added Home Page
    Added picture  for now

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,12 @@
 function Home() {
-    return <h1>Home</h1>
+    return(
+        <>
+       
+     <h1>Home</h1>
+     <img src="/Images/codewrap.jpg" alt="code"></img>
+     
+     </>
+    )
 }
 
 export default Home;
