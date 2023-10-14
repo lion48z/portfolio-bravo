8dc13626060501cab7f8970956d4dfecb1a83826
lion48z <140863653+lion48z@users.noreply.github.com>
2023-10-14 17:27:05 -0500

    adding sections
    updated for clickable page renders

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import  Navbar from './Navbar';
+import About from "./pages/About";
+import Education from "./pages/Education";
+import Experience from "./pages/Experience";
+import ContactMe from "./pages/ContactMe";
 
 function App() {
-  return (
-    
+  let Component 
+    switch (window.location.pathname) {
+      case "/":
+        Component = App
+        break
+      case "/aboutMe":
+        Component = About
+        break
+      case "/contactMe":
+        Component = ContactMe
+        break
+      case "education":
+        Component = Education
+        break
+      case "experience":
+        Component = Experience
+      
+    }
+    return (
+    <>
     <Navbar />
+    <Component />
+    </>
+    )
    
-  );
+  
 }
 
 export default App;
---
+++ src/pages/ContactMe.jsx
@@ -0,0 +1,5 @@
+function ContactMe() {
+    return <h1>Contact Me</h1>
+}
+
+export default ContactMe;
