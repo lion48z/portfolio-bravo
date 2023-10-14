487bb6ce71b4b7d4cd74824ef3005a83629eb689
lion48z <140863653+lion48z@users.noreply.github.com>
2023-10-14 16:09:09 -0500

    working Nav

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,14 +1,12 @@
 //have sections for about me, experience, education, contact me that jump to the section when clicked, have it designed so that 
 //more sections can be added in easily if updating information. 
 
-
-const Navbar = () =>{
 import 'bootstrap/dist/css/bootstrap.min.css';
 import  './styles.css';
 
-const  Navbar = () => {
+function Navbar() {
   return (
-    <nav className="row">
+    <nav className="nav">
       <a href="/" className="site-title">Home</a>
       <ul>
         <li>
@@ -27,18 +25,7 @@ const  Navbar = () => {
     </nav>
   );
 }
-}
+
 export default Navbar;
 
-
-const Navbar = () => {
-  const navigate = useNavigate();
-  return (
-    <div>
-      <h1 className="font-weight-light display-1 text-center" onClick={() => navigate('/')}>
-        The X app
-      </h1>
-    </div>
-  );
-};*/
 
