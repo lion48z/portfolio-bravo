227828c903e105de539aaaad4d5a84a93ca48ec4
Hope-Avritch <hopema66@gmail.com>
2023-10-18 19:24:22 -0400

    updating about me section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,37 +9,6 @@ function About()  {
             My goal is to continue learning and growing as a developer, taking on new challenges, and collaborating 
             with others to create meaningful and impactful web applications.</p>
     
-    
-
-My journey into web development began with a love for problem-solving and a desire to create innovative,
- user-friendly websites.
-
-I have a strong foundation in front-end development, with experience in creating responsive and visually 
-appealing user interfaces. 
-I'm also enthusiastic about back-end development and have worked on projects using technologies 
-like Node.js and Express.
-
-My goal is to continue learning and growing as a developer, taking on new challenges, and collaborating with others to create meaningful 
-and impactful web applications.
-
-### Skills
-
-- Front-end development: HTML, CSS, JavaScript, React
-- Back-end development: Node.js, Express
-- Version control with Git and GitHub
-- Responsive web design
-- Problem-solving and debugging
-
-
-
-My passion lies in creating seamless, user-centric experiences on the web. 
-Proficient in HTML, CSS, and JavaScript, I specialize in building responsive and dynamic front-end interfaces. 
-I'm not just about writing code; I'm dedicated to understanding user needs and translating them into elegant, 
-functional designs.
-
-
-
-    
     )
 }
 
