0c0ef9ae53dfa2e7ca4d8a136172adc0ace325d7
lion48z <140863653+lion48z@users.noreply.github.com>
2023-10-18 18:37:03 -0500

    Update Experience.jsx

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 function Experience() {
     return (
         <>
-        <h1>Education</h1>
+        <h1>Experience</h1>
         <div className="row">
             <div className="column">
-             <h2>Bachelors of Business Adminstration</h2>
+             <h2>Wordle</h2>
                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
                     sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
@@ -15,7 +15,7 @@ function Experience() {
         <br></br>
         <div className="row">
             <div className="column">
-            <h2>Eagle Corps Bootcamp</h2>
+            <h2>Tic Tac Toe</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
