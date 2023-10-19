871c24ab6c241d98ed756efdb5fab9480dce5390
lion48z <140863653+lion48z@users.noreply.github.com>
2023-10-19 18:18:52 -0500

    Update ContactMe.jsx
    adding variables

--- src/pages/ContactMe.jsx
+++ src/pages/ContactMe.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import { useState }from 'react;'
 function ContactMe() {
+    const ContactForm= () => {
+        const [submitted, setSubmitted] =useState(false);
+        const handleSubmit = (e) => e.preventDefault();
+    }
     return (
     <>
     <h1>Contact Me</h1>
