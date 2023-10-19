f5d0fbeb7b820619d4cabfc04d414f4213a68d16
lion48z <140863653+lion48z@users.noreply.github.com>
2023-10-18 20:37:19 -0500

    Update ContactMe.jsx

--- src/pages/ContactMe.jsx
+++ src/pages/ContactMe.jsx
@@ -1,6 +1,33 @@
 import React from 'react';
 function ContactMe() {
-    return <h1>Contact Me</h1>
+    return (
+    <>
+    <h1>Contact Me</h1>
+    <form>
+    <div className="row">
+            <div className="column">   
+    <input></input>
+    </div>
+      
+    
+            <div className="column">
+    <input></input>
+   
+        </div>
+
+            <div className="column">
+    <input></input>
+   
+        </div>
+    
+            <div className="column">
+    <input></input>
+    </div>
+        </div>
+    <button className="submit">submit</button>
+        
+    </form>
+    </>)
 }
 
 export default ContactMe;
