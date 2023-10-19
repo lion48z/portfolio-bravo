489076912b9a008747e59b3839531d7b78baddd3
lion48z <140863653+lion48z@users.noreply.github.com>
2023-10-18 21:23:27 -0500

    inputting stuff for form
    next step figure out useState for these sections

--- src/pages/ContactMe.jsx
+++ src/pages/ContactMe.jsx
@@ -4,25 +4,30 @@ function ContactMe() {
     <>
     <h1>Contact Me</h1>
     <form>
-    <div className="row">
-            <div className="column">   
-    <input></input>
-    </div>
+        <div className="row">
+            <div className="column"> 
+            <h5>Name</h5> 
+            <input></input>
+            
+            </div>
       
     
             <div className="column">
-    <input></input>
-   
-        </div>
+            <h5>Phone</h5> 
+            <input></input>
+        
+            </div>
 
             <div className="column">
-    <input></input>
+            <h5>Email</h5> 
+            <input></input>
    
-        </div>
+            </div>
     
             <div className="column">
-    <input></input>
-    </div>
+            <h5>Comments</h5> 
+            <input></input>
+            </div>
         </div>
     <button className="submit">submit</button>
         
