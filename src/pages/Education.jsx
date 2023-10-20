215f7a7f8c625bbde60091f7c0ce2d2c11f3012b
Hope-Avritch <hopema66@gmail.com>
2023-10-19 22:31:25 -0400

    update education.jsx

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -7,10 +7,14 @@ function Education() {
         <div className="row">
             <div className="column">
              <h2>Bachelors of Business Adminstration</h2>
-                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-                    sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                 <p> -Weber State 2006</p>
+                <h2>Eagle Corps Full Stack Boot Camp 2023</h2>
+                    <p>"The first coding program designed by veterans so veterans can successfully transition into a civilian career. Together, we lead the way."
+                    Gained knowledge from the Boot camp utilizing and running software such as: JavaScript, HTML, CSS, and React to create meaninfgul projects</p>
+                <h2>Free Code Camp Certificate Python 2022</h2>  
+                 <p>
+
+                 </p>
                 </div>
         </div>
         <br></br>
