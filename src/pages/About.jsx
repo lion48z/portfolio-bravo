778f0b1cc081a2b0da95ddcad2be5a3d31625dde
lion48z <140863653+lion48z@users.noreply.github.com>
2023-10-24 11:15:21 -0500

    Time saving styling
    Next focused area is the Experience section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,31 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from 'react-bootstrap';
 
+function About() {
+  const [typedText, setTypedText] = useState(''); // Initialize the state for typed text
+  const fullText =
+    "I'm Cody, a passionate junior web developer eager on contributing to the digital age. Currently, I have 3 years of experience working with a small company, completing front and back-end development. I gained knowledge and experience through the Eagle Corps Full-Stack Bootcamp. Such as learning the ways of: JavaScript, HTML, CSS, React, Python, and C++. My goal is to continue learning and growing as a developer, taking on new challenges, and collaborating with others to create meaningful and impactful web applications.";
 
-function About()  {
-    return (
-        <>
-         <div className="background" 
-            style={{ backgroundImage: `url('https://img.freepik.com/free-photo/abstract-glowing-flame-drops-electric-illumination-generative-ai_188544-8092.jpg?size=626&ext=jpg&ga=GA1.1.1274431862.1697766654&semt=sph')`,
-            backgroundSize: "100%",
-            color:"white",
-            minHeight: "100vh",
-   
+  useEffect(() => {
+    let index = 0;
+    const interval = setInterval(() => {
+      if (index < fullText.length) {
+        setTypedText((prevText) => prevText + fullText.charAt(index));
+        index++;
+      } else {
+        clearInterval(interval); // Stop the typing effect
+      }
+    }, 50); // Adjust typing speed here
 
-     }}>
-        <h1> About </h1>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRchYwXOq7D9cfT1OBYAeceshV-HRM0UO9wRL0upcFa3A&s" alt="businessman"align="right" width="400px" height="200px"></img>
-        <br></br>
-        <h3>I'm Cody, a passionate junior web developer eager on contributing to the digital age. 
-            Currently, I have 3 years experience working with a small company completing front and back end developing. 
-            I gained knowledge and experience through the Eagle Corps Full-Stack Bootcamp.
-            Such as learning the ways of: JavaScript, HTML, CSS, React, Python, and C++
-            My goal is to continue learning and growing as a developer, taking on new challenges, and collaborating 
-            with others to create meaningful and impactful web applications.</h3>
-     </div>
-   
-        </>
-    )
+    return () => clearInterval(interval); //clear 
+  }, []);
+
+  return (
+    <Container>
+      <Row>
+        <Col className="background" style={{ backgroundImage: `url('https://img.freepik.com/free-photo/abstract-glowing-flame-drops-electric-illumination-generative-ai_188544-8092.jpg?size=626&ext=jpg&ga=GA1.1.1274431862.1697766654&semt=sph')`, backgroundSize: "100%", color: "white", minHeight: "100vh" }}>
+          <h1> About </h1>
+          <img src="https://img.freepik.com/free-photo/cheerful-young-caucasian-businessman_171337-727.jpg?size=626&ext=jpg&ga=GA1.1.1274431862.1697766654&semt=ais" alt="businessman" align="right" width="400px" height="200px"></img>
+          <br></br>
+          <h3>{typedText}</h3>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default About;
+
