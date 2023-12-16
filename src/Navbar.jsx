91076f4657d8046b43cda4c66e5b0fe58d9b6d72
lion48z <140863653+lion48z@users.noreply.github.com>
2023-12-16 12:35:12 -0600

    added some comments

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,15 +3,15 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Nav, Container } from 'react-bootstrap';
-
+//Nav component 
 function Navigation() {
-  
+  //render Navbar with react-bootstrap library
 
   return (
     <Navbar bg="light" expand="lg">
       <Container>
         <Navbar.Brand href="/">North Mountain Dev LLC</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle aria-controls="basic-navbar-nav" /> {/*Navbar toggle responsive to use of small screens*/}
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
             <CustomLink href="/aboutMe">About Me</CustomLink>
@@ -24,6 +24,8 @@ function Navigation() {
     </Navbar>
   );
 }
+//custom link component to use for rendering custom links 
+//determines whether it is the active window or not 
 
 function CustomLink({ href, children }) {
   return (
